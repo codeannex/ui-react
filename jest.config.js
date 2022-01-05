9c21221c1d09fad693b670b1387666e39820d600
Aaron Balthaser <aaron@Aarons-MacBook-Pro-2.local>
2022-01-05 04:12:49 +0300

    update: updated jest to handle scss files

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,9 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>setupTests.js'],
+  moduleNameMapper: {
+    "^.+\\.(css|less|scss)$": "babel-jest"
+  },
   transform: {
     '^.+\\.(ts|tsx)?$': 'ts-jest',
     "^.+\\.(js|jsx)$": "babel-jest",
